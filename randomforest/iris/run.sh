04de7f4ab94dc7e0f2ef7b809a2403ea1a0a0e69
Mario Perez Esteso <mario@geekytheory.com>
2015-05-27 11:41:28 +0200

    Fixed path to Spark and added parameter 'master'

--- randomforest/iris/run.sh
+++ randomforest/iris/run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-/home/mario/Programs/spark-1.3.0-bin-hadoop2.4/bin/spark-submit --executor-memory 4g /home/mario/PycharmProjects/PySparkProjects/randomforest/iris/__init__.py
+rm -rf model/
+path/to/spark/bin/spark-submit --master local[2] --executor-memory 4g __init__.py
